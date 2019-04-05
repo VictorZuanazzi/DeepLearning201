801904e538e5b4af6a1d179727592149f2e8627a
Victor <victorzuanazzi@gmail.com>
2019-04-05 21:45:20 +0200

    fixed accuracy

--- assignment_1/code/train_convnet_pytorch.py
+++ assignment_1/code/train_convnet_pytorch.py
@@ -12,6 +12,8 @@ import numpy as np
 import os
 from convnet_pytorch import ConvNet
 import cifar10_utils
+import torch.optim as optim
+import torch
 
 # Default constants
 LEARNING_RATE_DEFAULT = 1e-4
@@ -22,6 +24,12 @@ OPTIMIZER_DEFAULT = 'ADAM'
 
 # Directory in which cifar data is saved
 DATA_DIR_DEFAULT = './cifar10/cifar-10-batches-py'
+
+#set datatype to torch tensor
+dtype = torch.FloatTensor
+
+#use GPUs if available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 FLAGS = None
 
@@ -38,16 +46,10 @@ def accuracy(predictions, targets):
   Returns:
     accuracy: scalar float, the accuracy of predictions,
               i.e. the average correct predictions over the whole batch
-  
-  TODO:
-  Implement accuracy computation.
   """
-    
-  #compares predictions to one-hot encodaed labels.
-  acc = np.multiply(predictions, targets)
   
   #calculates the mean accuracy over all predictions:
-  accuracy = acc.sum()/len(acc)
+  accuracy = (predictions.argmax(axis=1) == targets.argmax(axis=1)).mean()
 
   return accuracy
 
@@ -63,10 +65,51 @@ def train():
   ### DO NOT CHANGE SEEDS!
   # Set the random seeds for reproducibility
   np.random.seed(42)
-
-  ########################
-  # PUT YOUR CODE HERE  #
-  #######################
+  
+  #all external parameters in a readable format.
+  lr = FLAGS.learning_rate
+  max_steps = FLAGS.max_steps
+  batch_size = FLAGS.batch_size
+  eval_freq = FLAGS.eval_freq
+  data_dir = FLAGS.data_dir
+  
+  #fetch data
+  data = cifar10_utils.get_cifar10(FLAGS.data_dir)
+  n_classes = 10
+  n_channels = 3
+  
+  #load model
+  cnn_model = ConvNet(n_channels, n_classes)
+  
+  #Loss function
+  loss = torch.nn.CrossEntropyLoss()
+  
+  #Optimizer
+  optmizer = optim.Adam(cnn_model.parameters(), lr=lr)
+  
+  cnn_model.to(device)
+  #Train shit
+  for step in range(max_steps):
+      
+      #fetch next batch of data
+      X, y = data['train'].next_batch(batch_size)
+      X = torch.from_numpy(X).type(dtype).to(device)
+      y = torch.from_numpy(y).type(dtype).to(device)
+      
+      #reset gradient to zero before gradient descent.
+      optmizer.zero_grad()
+      
+      #calculate loss
+      probs = cnn_model.forward(X)
+      l = loss(probs, y.argmax(dim=1))
+      
+      #backward propagation
+      loss.backward()
+      optmizer.step()
+      
+      #calculate accuracy
+      acc = accuracy(probs, y)
+  
   raise NotImplementedError
   ########################
   # END OF YOUR CODE    #
