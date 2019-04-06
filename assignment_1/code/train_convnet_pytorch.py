d73d1a0fda5355c39ddfb3ae12ef06ad60c4360e
Victor <victorzuanazzi@gmail.com>
2019-04-06 17:26:33 +0200

    minimal working version

--- assignment_1/code/train_convnet_pytorch.py
+++ assignment_1/code/train_convnet_pytorch.py
@@ -18,7 +18,7 @@ import torch
 # Default constants
 LEARNING_RATE_DEFAULT = 1e-4
 BATCH_SIZE_DEFAULT = 32
-MAX_STEPS_DEFAULT = 50#5000
+MAX_STEPS_DEFAULT = 10#5000
 EVAL_FREQ_DEFAULT = 5#500
 OPTIMIZER_DEFAULT = 'ADAM'
 
@@ -34,24 +34,24 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 FLAGS = None
 
 def accuracy(predictions, targets):
-  """
-  Computes the prediction accuracy, i.e. the average of correct predictions
-  of the network.
-  
-  Args:
-    predictions: 2D float torch tensor of size [batch_size, n_classes]
-    labels: 2D int torch tensor of size [batch_size, n_classes]
+      """
+      Computes the prediction accuracy, i.e. the average of correct predictions
+      of the network.
+      
+      Args:
+          predictions: 2D float torch tensor of size [batch_size, n_classes]
+          labels: 2D int torch tensor of size [batch_size, n_classes]
             with one-hot encoding. Ground truth labels for
             each sample in the batch
-  Returns:
-    accuracy: scalar float, the accuracy of predictions,
+      Returns:
+          accuracy: scalar float, the accuracy of predictions,
               i.e. the average correct predictions over the whole batch
-  """
-  
-  #calculates the mean accuracy over all predictions:
-  accuracy = (predictions.argmax(dim=1) == targets.argmax(dim=1)).type(dtype).mean()
-
-  return accuracy.item() #returns the number instead of the tensor.
+      """
+      
+      #calculates the mean accuracy over all predictions:
+      accuracy = (predictions.argmax(dim=1) == targets.argmax(dim=1)).type(dtype).mean()
+    
+      return accuracy.detach().data.cpu().item() #returns the number instead of the tensor.
 
 def train():
   """
@@ -79,7 +79,7 @@ def train():
   n_channels = 3
   
   #number of iterations to train the data in the whole dataset:
-  n_iter = int(np.ceil(len(data["train"].labels)/batch_size))
+  n_iter = int(np.ceil(data["train"]._num_examples/batch_size))
   
   #load model
   cnn_model = ConvNet(n_channels, n_classes)
@@ -104,46 +104,75 @@ def train():
   
   #Train shit
   for s in range(max_steps):
+    print(f"step {s} out of {max_steps}")
+#    print(f"Memory Status")
+#    print(f"Memory cached: {torch.cuda.memory_cached()}")
+#    print(f"Memory allocated: {torch.cuda.memory_allocated()}")
+#    print(f"Max Memory cached: {torch.cuda.max_memory_cached()}")
+#    print(f"Max Memory allocated: {torch.cuda.max_memory_allocated()}")
       
     #go through the entire dataset
     for n in range(n_iter):
-          #fetch next batch of data
-          X, y = data['train'].next_batch(batch_size)
-          
-          #use torch tensor + gpu 
-          X = torch.from_numpy(X).type(dtype).to(device)
-          y = torch.from_numpy(y).type(dtype).to(device)
-          
-          #reset gradient to zero before gradient descent.
-          optmizer.zero_grad()
-          
-          #calculate loss
-          probs = cnn_model.forward(X)
-          loss = loss_XE(probs, y.argmax(dim=1)) 
-          
-          #backward propagation
-          loss.backward()
-          optmizer.step()
+      #fetch next batch of data
+      X, y = data['train'].next_batch(batch_size)
+      
+      #use torch tensor + gpu 
+      X = torch.from_numpy(X).type(dtype).to(device)
+      y = torch.from_numpy(y).type(dtype).to(device)
+      
+      #reset gradient to zero before gradient descent.
+      optmizer.zero_grad()
+      
+      #calculate loss
+      probs = cnn_model.forward(X)
+      loss = loss_XE(probs, y.argmax(dim=1)) 
+      
+      #backward propagation
+      loss.backward()
+      optmizer.step()
+      
+      #free GPU memory
+      loss.detach().data.cpu().item()
+      X.detach()
+      y.detach()
     
     #stores the loss and accuracy of the last batch for later analysis.
     l[s] = loss
     acc[s] = accuracy(probs, y)
-  
+    
+    probs.detach()
+    
     if s % eval_freq == 0:
         #calculate accuracy for the whole data set
         
-        #fetch all the data
-        X = data['train']._images
-        y = data['train']._labels
-      
-        #use torch tensor + gpu 
-        X = torch.from_numpy(X).type(dtype).to(device)
-        y = torch.from_numpy(y).type(dtype).to(device)
+        num_evals = int(np.ceil(data['test']._num_examples/batch_size))
         
-        #actually calculates loss and accuracy
-        probs = cnn_model.forward(X)
-        loss_all[eval_i] = loss_XE(probs, y.argmax(dim=1)) 
-        acc_all[eval_i] = accuracy(probs, y)
+        for t in range(num_evals):
+            #fetch all the data
+            X, y = data['test'].next_batch(batch_size)
+          
+            #use torch tensor + gpu 
+#            X = torch.from_numpy(X).type(dtype).to(device)
+#            y = torch.from_numpy(y).type(dtype).to(device)
+            
+            #releases the gradient to save memory
+            X = torch.tensor(X, requires_grad=False).type(dtype).to(device)
+            y = torch.tensor(y, requires_grad=False).type(dtype).to(device) 
+            
+            #actually calculates loss and accuracy for the batch
+            probs = cnn_model.forward(X)
+            loss_all[eval_i] += loss_XE(probs, y.argmax(dim=1)).detach().data.cpu().item()
+            acc_all[eval_i] += accuracy(probs, y)
+            
+            probs.detach()
+            
+            #frees memory
+            X.detach()
+            y.detach()
+        
+        #average the losses and accuracies across batches
+        loss_all[eval_i] /= num_evals
+        acc_all[eval_i] /= num_evals
         
         #increments eval counter
         eval_i +=1
@@ -164,13 +193,17 @@ def main():
   print_flags()
   
   #print the device being used.
-  print(device)
+  print("Device: ", device)
 
   if not os.path.exists(FLAGS.data_dir):
     os.makedirs(FLAGS.data_dir)
 
+  print("Step 1 of project Take Over the World:\nDistinguish cats from dogs.")
+
   # Run the training operation
   train()
+  
+  print("Training finished successfully. \nNote to self, cats are pure evil.")
 
 if __name__ == '__main__':
   # Command line arguments
