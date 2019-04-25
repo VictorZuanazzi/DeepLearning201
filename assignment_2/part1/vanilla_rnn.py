1853cf96eccd18d5c1aca6fda94053002cb2fe90
Victor <victorzuanazzi@gmail.com>
2019-04-25 20:58:58 +0200

    rnn, vanila please :icecream:

--- assignment_2/part1/vanilla_rnn.py
+++ assignment_2/part1/vanilla_rnn.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 
 import torch
 import torch.nn as nn
+import numpy as np
 
 ################################################################################
 
@@ -27,8 +28,46 @@ class VanillaRNN(nn.Module):
 
     def __init__(self, seq_length, input_dim, num_hidden, num_classes, batch_size, device='cpu'):
         super(VanillaRNN, self).__init__()
-        # Initialization here ...
+        
+        #Try script from Stijn after this thing is workin...
+        self.seq_length = seq_length
+        self.input_dim = input_dim
+        self.num_hidden = num_hidden
+        self.num_classes = num_classes
+        self.batch_size = batch_size
+        self.device = device
+        
+        #scale for initializing the weights
+        #inspired in how torch initializes its vectors
+        std = 1/num_hidden
+        
+        #Initialize Weight matrices
+        self.Wxh = nn.Parameter(std*torch.randn((input_dim, num_hidden)))
+        self.Whh = nn.Parameter(std*torch.randn((num_hidden, num_hidden)))
+        self.Whp = nn.Parameter(std*torch.randn((num_hidden, num_classes)))
+        
+        #Initialize Biases
+        self.bh = nn.Parameter(std*torch.randn(num_hidden))
+        self.bp = nn.Parameter(std*torch.randn(num_classes))
+        
+        #hidden state
+        self.h = torch.empty(batch_size, num_hidden, device=device)
+        
 
     def forward(self, x):
-        # Implementation here ...
-        pass
+        
+        #reset hidden state t0 zero before the forward pass
+        self.h.detach_()
+        nn.init.constant_(self.h, 0.0)
+        
+        #forward through all steps of the sequence
+        for t in range(self.seq_length):
+            xt = x[:,t].view(-1, self.input_dim)
+            hx = xt @ self.Wxh
+            hh = self.h @ self.Whh
+            self.h = torch.tanh(hx + hh + self.bh)
+         
+        #final linear layer
+        out = self.h @ self.Whp + self.bp
+        
+        return out
