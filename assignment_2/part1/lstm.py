745cdc4c23e77d9dbf33431550c48e7a8233b073
Victor <victorzuanazzi@gmail.com>
2019-04-26 16:13:29 +0200

    LSMT implemented :sunglasses:

--- assignment_2/part1/lstm.py
+++ assignment_2/part1/lstm.py
@@ -27,8 +27,105 @@ class LSTM(nn.Module):
 
     def __init__(self, seq_length, input_dim, num_hidden, num_classes, batch_size, device='cpu'):
         super(LSTM, self).__init__()
-        # Initialization here ...
+        
+        
+        #try Stijn's script once it is working
+        self.seq_length = seq_length
+        self.input_dim = input_dim
+        self.num_hidden = num_hidden
+        self.num_classes = num_classes
+        self.batch_size = batch_size
+        self.device = device
+        
+        #standard deviation for initizalization of weights
+        std = 1/num_hidden
+
+        #parameters of the Forget Gate
+        self.Wfx = nn.Parameter(std*torch.randn((input_dim, num_hidden)))
+        self.Wfh = nn.Parameter(std*torch.randn((num_hidden, num_hidden)))
+        self.bf = nn.Parameter(std*torch.randn(num_hidden))
+        
+        #parameters of the Input Gate
+        self.Wix = nn.Parameter(std*torch.randn((input_dim, num_hidden)))
+        self.Wih = nn.Parameter(std*torch.randn((num_hidden, num_hidden)))
+        self.bi = nn.Parameter(std*torch.randn(num_hidden))
+        
+        #paramters of the Modulation Gate
+        self.Wgx = nn.Parameter(std*torch.randn((input_dim, num_hidden)))
+        self.Wgh = nn.Parameter(std*torch.randn((num_hidden, num_hidden)))
+        self.bg = nn.Parameter(std*torch.randn(num_hidden))
+        
+        #paramters of the Output Gate
+        self.Wox = nn.Parameter(std*torch.randn((input_dim, num_hidden)))
+        self.Woh = nn.Parameter(std*torch.randn((num_hidden, num_hidden)))
+        self.bo = nn.Parameter(std*torch.randn(num_hidden))
+        
+        #hidden and cell states:
+        self.c = torch.empty(batch_size, num_hidden, device = device)
+        self.h = torch.empty(batch_size, num_hidden, device = device)
+        
+        #Linear layer
+        self.Wph = nn.Parameter(std*torch.randn((num_hidden, num_classes)))
+        self.bp = nn.Parameter(std*torch.randn(num_classes))
+
+    def reset(self):
+        
+        #reset cell state
+        self.c.detach_()
+        nn.init.constant_(self.c, 0.0)
+        
+        #reset hidden state
+        self.h.detach_()
+        nn.init.constant_(self.h, 0.0)
 
     def forward(self, x):
-        # Implementation here ...
-        pass
+        
+        #reset cell and hidden state before starting the sequence
+        self.reset()
+        
+        #forward pass through the sequence
+        for t in range(self.seq_length):
+            xt = x[:, t].view(self.batch_size, self.input_dim)
+            
+            g = torch.tanh(xt @ self.Wgx + self.h @ self.Wgh + self.bg) #eq 4
+            i = torch.sigmoid(xt @ self.Wix + self.h @ self.Wih + self.bi) #eq 5
+            f = torch.sigmoid(xt @ self.Wfx + self.h @ self.Wfh + self.bf) #eq 6
+            o = torch.sigmoid(xt @ self.Wox + self.h @ self.Woh + self.bo) #eq 7
+            
+            self.c = g * i + self.c * f #eq 8
+            self.h = torch.tanh(self.c) * o #eq 9
+            
+        #linear foward step
+        p = self.h @ self.Wph + self.bp #eq 10
+        
+        return p
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
